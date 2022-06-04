b8e809af1dd894f4c7de28dbc9dd969e9e8e51ce
Robert Cabrera <cabrera.rch@gmail.com>
2022-06-04 18:55:23 +0200

    feat: config to save images in cloudinary

--- config/routes.js
+++ config/routes.js
@@ -2,9 +2,10 @@ const express = require("express");
 const router = express.Router();
 const productController = require("../controllers/products.controller")
 
+const upload = require("./storage.config")
 
 /* ROUTES */
 router.get("/", (req, res, next) => res.status(200).json({ ok: true}))
-router.post("/products/new", productController.create)
+router.post("/new-product", upload.single('image'), productController.create)
 
 module.exports = router
--- controllers/products.controller.js
+++ controllers/products.controller.js
@@ -2,7 +2,10 @@ const Product = require("../models/Product.model");
 
 module.exports.create = (req, res, next) => {
   const product = { title, description } = req.body
-  console.log(product)
+
+  if (req.file) {
+    product.image = req.file.path
+  }
 
   Product.create(product)
     .then(newProduct => res.status(200).json(newProduct))
